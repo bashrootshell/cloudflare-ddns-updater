1d1b252a24d7d46c9dfeac51ab7ac02234c1e5cc
bashrootshell <cicero.sec@gmail.com>
2021-02-19 08:37:53 -0300

    update

--- cloudflare-ddns-updater.sh
+++ cloudflare-ddns-updater.sh
@@ -5,6 +5,7 @@
 ## Create a crontab entry for this file so it can run from time to time.
 ## Ex: * */6 * * * /home/user/cloudflare-ddns-updater.sh > /home/user/ddns.log
 
+file=./current_ip_address.txt
 current_ip_address=$(curl -s https://api.ipify.org)
 unixtime=$(date +%s)
 api_url='https://api.cloudflare.com/client/v4/zones'
@@ -12,10 +13,10 @@ domain_zone='REPLACE'
 dns_record='REPLACE'
 # use API Tokens (https://developers.cloudflare.com/api/keys)
 # "API Keys are the legacy authorization scheme for talking to Cloudflare's APIs."
-token='Bearer REPLACE VALUE'
+token='Bearer API_TOKEN'
 
-if [ -f ./current_ip_address ]; then
-  if grep -q "$current_ip_address" current_ip_address; then
+if [ -f $file ]; then
+  if grep -q "$current_ip_address" $file; then
       echo "$unixtime > Same IP address: $current_ip_address"
       exit 1
   else
@@ -23,11 +24,11 @@ if [ -f ./current_ip_address ]; then
     -H "Authorization: $token" \
     -H "Content-Type: application/json" \
     --data '{"type":"A","name":"A_RECORD","content":"'$current_ip_address'","ttl":1,"proxied":true}'
-    echo $current_ip_address > current_ip_address
+    echo $current_ip_address > $file
     echo "timestamp: $unixtime  |  New IP Address: $current_ip_address"
     exit 0
   fi
 else
-  echo $ipaddr > current_ip_address
+  echo $current_ip_address > $file
   exit 1
 fi
